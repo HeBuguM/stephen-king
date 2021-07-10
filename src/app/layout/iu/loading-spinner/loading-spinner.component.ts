bb16b87296e139fd0fc0a91f3a58a3d7e8b5e65d
Alexander Angelov <HeBuguM@gmail.com>
2021-07-10 12:42:10 +0300

    book loading

--- src/app/layout/iu/loading-spinner/loading-spinner.component.ts
+++ src/app/layout/iu/loading-spinner/loading-spinner.component.ts
@@ -1,10 +1,9 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-loading-spinner',
   templateUrl: './loading-spinner.component.html',
-  styleUrls: ['./loading-spinner.component.css'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./loading-spinner.component.css']
 })
 export class LoadingSpinnerComponent implements OnInit {
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -15,14 +15,5 @@ export const environment = {
 	},
 	useHash: false,
 	appBaseHref: '/',
-	dataBaseHref: 'https://sk.adventurersbg.info/'
+	dataBaseHref: 'http://localhost/sk/'
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
